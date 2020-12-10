cbf1ed90c75eba1a0d207f336584d97968a17904
xuyang <424629123@qq.com>
2020-12-10 15:46:31 +0800

    remove appjs

--- app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http'); 
-console.log("Kubia server starting... ") ; 
-var handler = function(request, response) {
-	console.log("Received request from " + request.connection.remoteAddress); 
-	response.writeHead(200); 
-	response.end("hello");
-}
-var www = http.createServer(handler); 
-www.listen(8080);
